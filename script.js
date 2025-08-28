b135dccea7b8aeed54c63abb198b4cc678473fa1
Arafat Shalehin <md.arafatshalehin@gmail.com>
2025-08-28 20:50:05 +0600

    Completed Call History Section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const callBtnData = [];
+
 // Reuaseble Code with call and coin functionalities
 
 function validCallCheck (title, num) {
@@ -19,9 +21,52 @@ function validCallCheck (title, num) {
 
     alert(`${mainTitle}\n${number}`);
 
+    const data = {
+        name: mainTitle,
+        num: number,
+        date: new Date().toLocaleTimeString()
+    }
+
+    callBtnData.push(data);
+
     return coinBtn.innerText = finalCoinCount;
 
 }
+
+
+// Reuaseble Code for Call History
+
+function makeCallHistory () {
+
+    const callHistoryContainer = document.getElementById("call-history-card-container");
+
+    callHistoryContainer.innerText = "";
+
+    for(data of callBtnData) {
+        const div = document.createElement("div")
+
+        div.innerHTML = `
+        <div class="
+
+            bg-[#e9e8e8] sm:w-[500px] sm:h-[80px] w-[320px]
+            mx-auto rounded-xl sm:mt-5 mt-3
+            flex justify-between items-center
+            ">
+            <div class="p-2 sm:p-9">
+                <h2 class="font-bold mt-3 text-xl">${data.name}</h2>
+                <p class="text-[#8C8C8C] text-xl">${data.num}</p>
+            </div>
+
+            <p class="text-lg p-2 sm:p-9">${data.date}</p>
+
+        </div>
+        `
+
+        callHistoryContainer.appendChild(div);
+    }
+
+};
+
 
 // Heart Section Functionalities.
 
@@ -49,17 +94,23 @@ document.getElementById("national-call-btn").addEventListener("click", function 
 
     validCallCheck("national-title", "national-num");
 
+    makeCallHistory();
+
 });
 
 document.getElementById("police-call-btn").addEventListener("click", function () {
 
     validCallCheck("police-title", "police-num");
 
+    makeCallHistory();
+
 });
 
 document.getElementById("fire-call-btn").addEventListener("click", function () {
 
     validCallCheck("fire-title", "fire-num");
+
+    makeCallHistory();
 
 });
 
@@ -68,17 +119,23 @@ document.getElementById("ambulance-call-btn").addEventListener("click", function
 
     validCallCheck("ambulance-title", "ambulance-num");
 
+    makeCallHistory();
+
 });
 
 document.getElementById("women-call-btn").addEventListener("click", function () {
 
     validCallCheck("women-title", "women-num");
 
+    makeCallHistory();
+
 });
 
 document.getElementById("anti-call-btn").addEventListener("click", function () {
 
     validCallCheck("anti-title", "anti-num");
+
+    makeCallHistory();
 
 });
 
@@ -87,16 +144,22 @@ document.getElementById("ele-call-btn").addEventListener("click", function () {
 
     validCallCheck("ele-title", "ele-num");
 
+    makeCallHistory();
+
 });
 
 document.getElementById("brac-call-btn").addEventListener("click", function () {
 
     validCallCheck("brac-title", "brac-num");
 
+    makeCallHistory();
+
 });
 
 document.getElementById("bdr-call-btn").addEventListener("click", function () {
 
     validCallCheck("bdr-title", "bdr-num");
+
+    makeCallHistory();
 
 });
